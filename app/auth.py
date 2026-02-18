6092c2e29d967379d4984d653b44e945e27ce3cf
Titi0512 <125388033+Titi0512@users.noreply.github.com>
2026-02-18 14:03:23 +0700

    Fix password verification logic in verify_password

--- app/auth.py
+++ app/auth.py
@@ -22,7 +22,7 @@ class TokenData(BaseModel):
     username: Optional[str] = None
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(hashed_password)
+    return pwd_context.verify(plain_password,hashed_password)
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
